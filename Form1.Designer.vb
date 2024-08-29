45fbd00c586bfaae173c11fc117dd08b4dc8f2c4
nobody <nobody@DESKTOP-F6OAEGL>
2024-08-29 18:49:24 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,347 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.BtnCalculate = New System.Windows.Forms.Button()
+		Me.btnClear = New System.Windows.Forms.Button()
+		Me.btnExit = New System.Windows.Forms.Button()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.radSmartphone = New System.Windows.Forms.RadioButton()
+		Me.radTablet = New System.Windows.Forms.RadioButton()
+		Me.radLaptop = New System.Windows.Forms.RadioButton()
+		Me.radPersonalComputer = New System.Windows.Forms.RadioButton()
+		Me.chkComputer = New System.Windows.Forms.CheckBox()
+		Me.chkGraphicsCard = New System.Windows.Forms.CheckBox()
+		Me.chkMonitor = New System.Windows.Forms.CheckBox()
+		Me.chkKeyboard = New System.Windows.Forms.CheckBox()
+		Me.cmbAdditionalRam = New System.Windows.Forms.ComboBox()
+		Me.lblName = New System.Windows.Forms.Label()
+		Me.lblSurname = New System.Windows.Forms.Label()
+		Me.txtName = New System.Windows.Forms.TextBox()
+		Me.txtSurname = New System.Windows.Forms.TextBox()
+		Me.chkMouse = New System.Windows.Forms.CheckBox()
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+		Me.cmbAdditionalDDR4 = New System.Windows.Forms.ComboBox()
+		Me.lblAdditionalRam = New System.Windows.Forms.Label()
+		Me.lblAdditionalDDR4 = New System.Windows.Forms.Label()
+		Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+		Me.lstdisplay = New System.Windows.Forms.ListBox()
+		Me.GroupBox1.SuspendLayout()
+		Me.GroupBox2.SuspendLayout()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'BtnCalculate
+		'
+		Me.BtnCalculate.Location = New System.Drawing.Point(297, 517)
+		Me.BtnCalculate.Name = "BtnCalculate"
+		Me.BtnCalculate.Size = New System.Drawing.Size(164, 23)
+		Me.BtnCalculate.TabIndex = 0
+		Me.BtnCalculate.Text = "Calculate"
+		Me.BtnCalculate.UseVisualStyleBackColor = True
+		'
+		'btnClear
+		'
+		Me.btnClear.Location = New System.Drawing.Point(549, 517)
+		Me.btnClear.Name = "btnClear"
+		Me.btnClear.Size = New System.Drawing.Size(185, 23)
+		Me.btnClear.TabIndex = 1
+		Me.btnClear.Text = "Clear"
+		Me.btnClear.UseVisualStyleBackColor = True
+		'
+		'btnExit
+		'
+		Me.btnExit.Location = New System.Drawing.Point(816, 517)
+		Me.btnExit.Name = "btnExit"
+		Me.btnExit.Size = New System.Drawing.Size(189, 23)
+		Me.btnExit.TabIndex = 2
+		Me.btnExit.Text = "Exit"
+		Me.btnExit.UseVisualStyleBackColor = True
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.radSmartphone)
+		Me.GroupBox1.Controls.Add(Me.radTablet)
+		Me.GroupBox1.Controls.Add(Me.radLaptop)
+		Me.GroupBox1.Controls.Add(Me.radPersonalComputer)
+		Me.GroupBox1.Location = New System.Drawing.Point(297, 12)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(708, 73)
+		Me.GroupBox1.TabIndex = 7
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "Computing Devices"
+		'
+		'radSmartphone
+		'
+		Me.radSmartphone.AutoSize = True
+		Me.radSmartphone.Location = New System.Drawing.Point(599, 34)
+		Me.radSmartphone.Name = "radSmartphone"
+		Me.radSmartphone.Size = New System.Drawing.Size(82, 17)
+		Me.radSmartphone.TabIndex = 3
+		Me.radSmartphone.TabStop = True
+		Me.radSmartphone.Text = "Smartphone"
+		Me.radSmartphone.UseVisualStyleBackColor = True
+		'
+		'radTablet
+		'
+		Me.radTablet.AutoSize = True
+		Me.radTablet.Location = New System.Drawing.Point(418, 34)
+		Me.radTablet.Name = "radTablet"
+		Me.radTablet.Size = New System.Drawing.Size(55, 17)
+		Me.radTablet.TabIndex = 2
+		Me.radTablet.TabStop = True
+		Me.radTablet.Text = "Tablet"
+		Me.radTablet.UseVisualStyleBackColor = True
+		'
+		'radLaptop
+		'
+		Me.radLaptop.AutoSize = True
+		Me.radLaptop.Location = New System.Drawing.Point(231, 34)
+		Me.radLaptop.Name = "radLaptop"
+		Me.radLaptop.Size = New System.Drawing.Size(58, 17)
+		Me.radLaptop.TabIndex = 1
+		Me.radLaptop.TabStop = True
+		Me.radLaptop.Text = "Laptop"
+		Me.radLaptop.UseVisualStyleBackColor = True
+		'
+		'radPersonalComputer
+		'
+		Me.radPersonalComputer.AutoSize = True
+		Me.radPersonalComputer.Location = New System.Drawing.Point(26, 34)
+		Me.radPersonalComputer.Name = "radPersonalComputer"
+		Me.radPersonalComputer.Size = New System.Drawing.Size(114, 17)
+		Me.radPersonalComputer.TabIndex = 0
+		Me.radPersonalComputer.TabStop = True
+		Me.radPersonalComputer.Text = "Personal Computer"
+		Me.radPersonalComputer.UseVisualStyleBackColor = True
+		'
+		'chkComputer
+		'
+		Me.chkComputer.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.chkComputer.Location = New System.Drawing.Point(69, 31)
+		Me.chkComputer.Name = "chkComputer"
+		Me.chkComputer.Size = New System.Drawing.Size(107, 27)
+		Me.chkComputer.TabIndex = 8
+		Me.chkComputer.Text = "Computer"
+		Me.chkComputer.UseVisualStyleBackColor = True
+		'
+		'chkGraphicsCard
+		'
+		Me.chkGraphicsCard.AutoSize = True
+		Me.chkGraphicsCard.Location = New System.Drawing.Point(22, 79)
+		Me.chkGraphicsCard.Name = "chkGraphicsCard"
+		Me.chkGraphicsCard.Size = New System.Drawing.Size(93, 17)
+		Me.chkGraphicsCard.TabIndex = 9
+		Me.chkGraphicsCard.Text = "Graphics Card"
+		Me.chkGraphicsCard.UseVisualStyleBackColor = True
+		'
+		'chkMonitor
+		'
+		Me.chkMonitor.AutoSize = True
+		Me.chkMonitor.Location = New System.Drawing.Point(165, 79)
+		Me.chkMonitor.Name = "chkMonitor"
+		Me.chkMonitor.Size = New System.Drawing.Size(61, 17)
+		Me.chkMonitor.TabIndex = 10
+		Me.chkMonitor.Text = "Monitor"
+		Me.chkMonitor.UseVisualStyleBackColor = True
+		'
+		'chkKeyboard
+		'
+		Me.chkKeyboard.AutoSize = True
+		Me.chkKeyboard.Location = New System.Drawing.Point(22, 123)
+		Me.chkKeyboard.Name = "chkKeyboard"
+		Me.chkKeyboard.Size = New System.Drawing.Size(71, 17)
+		Me.chkKeyboard.TabIndex = 11
+		Me.chkKeyboard.Text = "Keyboard"
+		Me.chkKeyboard.UseVisualStyleBackColor = True
+		'
+		'cmbAdditionalRam
+		'
+		Me.cmbAdditionalRam.FormattingEnabled = True
+		Me.cmbAdditionalRam.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5"})
+		Me.cmbAdditionalRam.Location = New System.Drawing.Point(132, 209)
+		Me.cmbAdditionalRam.Name = "cmbAdditionalRam"
+		Me.cmbAdditionalRam.Size = New System.Drawing.Size(94, 21)
+		Me.cmbAdditionalRam.TabIndex = 12
+		'
+		'lblName
+		'
+		Me.lblName.AutoSize = True
+		Me.lblName.Location = New System.Drawing.Point(12, 225)
+		Me.lblName.Name = "lblName"
+		Me.lblName.Size = New System.Drawing.Size(35, 13)
+		Me.lblName.TabIndex = 13
+		Me.lblName.Text = "Name"
+		'
+		'lblSurname
+		'
+		Me.lblSurname.AutoSize = True
+		Me.lblSurname.Location = New System.Drawing.Point(12, 261)
+		Me.lblSurname.Name = "lblSurname"
+		Me.lblSurname.Size = New System.Drawing.Size(49, 13)
+		Me.lblSurname.TabIndex = 14
+		Me.lblSurname.Text = "Surname"
+		'
+		'txtName
+		'
+		Me.txtName.Location = New System.Drawing.Point(81, 218)
+		Me.txtName.Name = "txtName"
+		Me.txtName.Size = New System.Drawing.Size(188, 20)
+		Me.txtName.TabIndex = 15
+		'
+		'txtSurname
+		'
+		Me.txtSurname.Location = New System.Drawing.Point(81, 258)
+		Me.txtSurname.Name = "txtSurname"
+		Me.txtSurname.Size = New System.Drawing.Size(188, 20)
+		Me.txtSurname.TabIndex = 16
+		'
+		'chkMouse
+		'
+		Me.chkMouse.AutoSize = True
+		Me.chkMouse.Location = New System.Drawing.Point(165, 123)
+		Me.chkMouse.Name = "chkMouse"
+		Me.chkMouse.Size = New System.Drawing.Size(58, 17)
+		Me.chkMouse.TabIndex = 17
+		Me.chkMouse.Text = "Mouse"
+		Me.chkMouse.UseVisualStyleBackColor = True
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.cmbAdditionalDDR4)
+		Me.GroupBox2.Controls.Add(Me.lblAdditionalRam)
+		Me.GroupBox2.Controls.Add(Me.lblAdditionalDDR4)
+		Me.GroupBox2.Controls.Add(Me.chkComputer)
+		Me.GroupBox2.Controls.Add(Me.chkMouse)
+		Me.GroupBox2.Controls.Add(Me.cmbAdditionalRam)
+		Me.GroupBox2.Controls.Add(Me.chkGraphicsCard)
+		Me.GroupBox2.Controls.Add(Me.chkMonitor)
+		Me.GroupBox2.Controls.Add(Me.chkKeyboard)
+		Me.GroupBox2.Location = New System.Drawing.Point(12, 284)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(257, 250)
+		Me.GroupBox2.TabIndex = 18
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "Computer and Peripherals"
+		'
+		'cmbAdditionalDDR4
+		'
+		Me.cmbAdditionalDDR4.FormattingEnabled = True
+		Me.cmbAdditionalDDR4.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5"})
+		Me.cmbAdditionalDDR4.Location = New System.Drawing.Point(132, 167)
+		Me.cmbAdditionalDDR4.Name = "cmbAdditionalDDR4"
+		Me.cmbAdditionalDDR4.Size = New System.Drawing.Size(94, 21)
+		Me.cmbAdditionalDDR4.TabIndex = 20
+		'
+		'lblAdditionalRam
+		'
+		Me.lblAdditionalRam.AutoSize = True
+		Me.lblAdditionalRam.Location = New System.Drawing.Point(19, 217)
+		Me.lblAdditionalRam.Name = "lblAdditionalRam"
+		Me.lblAdditionalRam.Size = New System.Drawing.Size(80, 13)
+		Me.lblAdditionalRam.TabIndex = 19
+		Me.lblAdditionalRam.Text = "Additional HDD"
+		'
+		'lblAdditionalDDR4
+		'
+		Me.lblAdditionalDDR4.AutoSize = True
+		Me.lblAdditionalDDR4.Location = New System.Drawing.Point(19, 170)
+		Me.lblAdditionalDDR4.Name = "lblAdditionalDDR4"
+		Me.lblAdditionalDDR4.Size = New System.Drawing.Size(86, 13)
+		Me.lblAdditionalDDR4.TabIndex = 18
+		Me.lblAdditionalDDR4.Text = "Additional DDR4"
+		'
+		'PictureBox2
+		'
+		Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+		Me.PictureBox2.InitialImage = Nothing
+		Me.PictureBox2.Location = New System.Drawing.Point(12, 12)
+		Me.PictureBox2.Name = "PictureBox2"
+		Me.PictureBox2.Size = New System.Drawing.Size(257, 187)
+		Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox2.TabIndex = 20
+		Me.PictureBox2.TabStop = False
+		'
+		'lstdisplay
+		'
+		Me.lstdisplay.FormattingEnabled = True
+		Me.lstdisplay.Location = New System.Drawing.Point(297, 91)
+		Me.lstdisplay.Name = "lstdisplay"
+		Me.lstdisplay.Size = New System.Drawing.Size(708, 420)
+		Me.lstdisplay.TabIndex = 21
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1017, 551)
+		Me.Controls.Add(Me.lstdisplay)
+		Me.Controls.Add(Me.PictureBox2)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.Controls.Add(Me.txtSurname)
+		Me.Controls.Add(Me.txtName)
+		Me.Controls.Add(Me.lblSurname)
+		Me.Controls.Add(Me.lblName)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Controls.Add(Me.btnExit)
+		Me.Controls.Add(Me.btnClear)
+		Me.Controls.Add(Me.BtnCalculate)
+		Me.Name = "Form1"
+		Me.Text = "Bongani Computers Logged"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox2.PerformLayout()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents BtnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents radSmartphone As RadioButton
+    Friend WithEvents radTablet As RadioButton
+    Friend WithEvents radLaptop As RadioButton
+    Friend WithEvents radPersonalComputer As RadioButton
+    Friend WithEvents chkComputer As CheckBox
+    Friend WithEvents chkGraphicsCard As CheckBox
+    Friend WithEvents chkMonitor As CheckBox
+    Friend WithEvents chkKeyboard As CheckBox
+    Friend WithEvents cmbAdditionalRam As ComboBox
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblSurname As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents chkMouse As CheckBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblAdditionalRam As Label
+    Friend WithEvents lblAdditionalDDR4 As Label
+    Friend WithEvents cmbAdditionalDDR4 As ComboBox
+    Friend WithEvents PictureBox2 As PictureBox
+	Friend WithEvents lstdisplay As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,262 @@
+﻿' Bongani Computers Source Code
+Imports System.Globalization
+Public Class Form1
+    Dim strName As String
+    Dim strSurname As String
+
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles BtnCalculate.Click
+        Dim decSubtotal As Decimal
+        Dim decVatRate As Decimal = 0.15
+        Dim decVatAmount As Decimal
+        Dim dectotal As Decimal
+        Dim decTotalMarkup As Decimal
+
+        'Conversion of Variables  to Control Names
+        strName = CStr(txtName.Text)
+        strSurname = CStr(txtSurname.Text)
+
+        lstdisplay.Items.Clear()
+
+        'Display the Output
+        lstdisplay.Items.Add("QUOTATION for" + " " + strSurname + "," + strName)
+        lstdisplay.Items.Add("___________________________________________________________________________________________________________________")
+
+        'Condition Statements for Computing Devices Radio Buttons
+        If radPersonalComputer.Checked = True Then
+            lstdisplay.Items.Add("Personal Computer:Bongani Computers")                                                                                                                                                                                                                      
+            lstdisplay.Items.Add("System board: B550M Pro4 AM4")
+            lstdisplay.Items.Add("CPU: Intel S1200 Core i5 11400 TRAY 6*26 65W 11Gen @ 4.4 GHz")
+            lstdisplay.Items.Add("RAM: DDR4")
+            lstdisplay.Items.Add("HDD: 1TB")
+            lstdisplay.Items.Add(" " + "R6009.35".PadLeft(220))
+
+            decSubtotal += 6009.35D
+            decTotalMarkup += 6009.35D * 0.4
+
+
+        ElseIf radLaptop.Checked = True Then
+            lstdisplay.Items.Add("Laptop:Dell Latitude 5420 intel core i5")                                                                                                                                                                                                         
+            lstdisplay.Items.Add("CPU 11th Gen Core i5-1185G7 3.0GHz")
+            lstdisplay.Items.Add("SSD: 256GB M.2 Solid State Drive SSD")
+            lstdisplay.Items.Add("RAM: 8 GB DDR4 3200 MHz Memory")
+            lstdisplay.Items.Add("Screen Length: 14 inch FHD 1920 x 1080 display")
+            lstdisplay.Items.Add("Graphics Card: Integrated Intel Iris Xe Graphics")
+            lstdisplay.Items.Add("Operating System: Windows 10 Pro Operating System")
+            lstdisplay.Items.Add("Application Software: Microsoft Office 2019 Pro Plus")
+            lstdisplay.Items.Add(" " + "R8500.00".PadLeft(220))
+
+            decSubtotal += 8500D
+            decTotalMarkup += 8500D * 0.4
+
+        ElseIf radTablet.Checked = True Then
+            lstdisplay.Items.Add("Tablet:Samsung Galaxy Tab A9+ 5G 64GB Graphite")                                                                                                                                                                                     
+            lstdisplay.Items.Add("Screen: 11 TFT (1920x1200:90Hz) WUXGA")
+            lstdisplay.Items.Add("Storage and Memory: 64GB storage and 4GB RAM")
+            lstdisplay.Items.Add("Camera: Rear 8MP")
+            lstdisplay.Items.Add("Camera: Front 5MP")
+            lstdisplay.Items.Add("Battery: 7040mAh")
+            lstdisplay.Items.Add("OS: Android 13")
+            lstdisplay.Items.Add("Security: Face recognition, PIN, Password")
+            lstdisplay.Items.Add("Warranty: 2 year warranty")
+            lstdisplay.Items.Add(" " + "R4999.00".PadLeft(220))
+
+            decSubtotal += 4999D
+            decTotalMarkup += 4999D * 0.4
+
+        ElseIf radSmartphone.Checked = True Then
+            lstdisplay.Items.Add("Smartphone:SAMSUNG A05s 64GB SM-A057F/DS LIGHT VIOLET LTE AFA")                                                                                                                                                               
+            lstdisplay.Items.Add("Screen: 6.5-inch FHD+ display")
+            lstdisplay.Items.Add("Memory: MicroSD expandable up to 1TB")
+            lstdisplay.Items.Add("Charger: C-type super fast 25W charging support")
+            lstdisplay.Items.Add("Camera:  50MP (F1.8) main camera with auto focus + 2MP (F2.4) depth camera + 2MP (F2.4) macro camera, 13MP (F2.0) front camera")
+            lstdisplay.Items.Add("Processor: Qualcomm Snapdragon 680 Processor 2.4GHz, 1.9GHz 12nm Octa-Processor")
+            lstdisplay.Items.Add(" " + "R2545.00".PadLeft(220))
+
+            decSubtotal += 2545D
+            decTotalMarkup += 2545D * 0.4
+        End If
+
+
+        'Condition Statements for Computer & Peripherals Checkboxes
+        'Computer
+        If chkComputer.Checked = True Then
+            lstdisplay.Items.Add("Personal Computer:Bongani Computers")
+            lstdisplay.Items.Add("System board: B550M Pro4 AM4")
+            lstdisplay.Items.Add("CPU: Intel S1200 Core i5 11400 TRAY 6*26 65W 11Gen @ 4.4 GHz")
+            lstdisplay.Items.Add("RAM: DDR4")
+            lstdisplay.Items.Add("HDD: 1TB")
+            lstdisplay.Items.Add(" " + "R6009.35".PadLeft(220))
+
+            decSubtotal += 6009.35D
+            decTotalMarkup += 6009.35D * 0.4
+        End If
+
+
+        'GraphicsCard
+        If chkGraphicsCard.Checked = True Then
+            lstdisplay.Items.Add("Graphics card")
+            lstdisplay.Items.Add(" " + "R2607.83".PadLeft(220))
+
+            decSubtotal += 2545.83D
+            decTotalMarkup += 2545.83D * 0.4
+        End If
+
+        'Monitor
+        If chkMonitor.Checked = True Then
+            lstdisplay.Items.Add("Monitor")
+            lstdisplay.Items.Add(" " + "R520.87".PadLeft(221))
+
+            decSubtotal += 520.87D
+            decTotalMarkup += 520.87D * 0.4
+        End If
+
+        'Keyboard
+        If chkKeyboard.Checked = True Then
+            lstdisplay.Items.Add("Keyboard")
+            lstdisplay.Items.Add(" " + "R134.00".PadLeft(221))
+
+            decSubtotal += 134D
+            decTotalMarkup += 134D * 0.4
+        End If
+
+        'Mouse
+        If chkMouse.Checked = True Then
+            lstdisplay.Items.Add("Mouse")
+            lstdisplay.Items.Add(" " + "R86.09".PadLeft(222))
+
+            decSubtotal += 86.09D
+            decTotalMarkup += 86.09D * 0.4
+        End If
+
+
+
+        'Condition Statements for Computer & Peripherals Comboboxes
+        'Additional DDR4 RAM
+        Select Case cmbAdditionalDDR4.SelectedIndex
+            Case 0
+                lstdisplay.Items.Add("Additional Ram 0 x 1580.87")
+                lstdisplay.Items.Add(" " + "R0.00".PadLeft(223))
+
+                decSubtotal += 0.00D
+                decTotalMarkup += 0.00D * 0.4
+            Case 1
+                lstdisplay.Items.Add("Additional Ram 1 x 1580.87")
+                lstdisplay.Items.Add(" " + "R1580.87".PadLeft(220))
+
+                decSubtotal += 1580.87D
+                decTotalMarkup += 1580.87D * 0.4
+            Case 2
+                lstdisplay.Items.Add("Additional Ram 2 x 1580.87")
+                lstdisplay.Items.Add(" " + "R3161.74".PadLeft(220))
+
+                decSubtotal += 3161.74D
+                decTotalMarkup += 3161.74D * 0.4
+            Case 3
+                lstdisplay.Items.Add("Additional Ram 3 x 1580.87")
+                lstdisplay.Items.Add(" " + "R4742.61".PadLeft(220))
+
+                decSubtotal += 4742.61D
+                decTotalMarkup += 4742.61D * 0.4
+            Case 4
+                lstdisplay.Items.Add("Additional Ram 4 x 1580.87")
+                lstdisplay.Items.Add(" " + "R6323.48".PadLeft(220))
+
+                decSubtotal += 6323.48D
+                decTotalMarkup += 6323.48D * 0.4
+            Case 5
+                lstdisplay.Items.Add("Additional Ram 5 x 1580.87")
+                lstdisplay.Items.Add(" " + "R7904.35".PadLeft(220))
+
+                decSubtotal += 7904.35D
+                decTotalMarkup += 7904.35D * 0.4
+        End Select
+
+        'Additional HDD
+        Select Case cmbAdditionalRam.SelectedIndex
+            Case 0
+                lstdisplay.Items.Add("Additional HDD 0 x 583.48")
+                lstdisplay.Items.Add(" " + "R0.00".PadLeft(223))
+
+                decSubtotal += 0.00D
+                decTotalMarkup += 0.00D * 0.4
+            Case 1
+                lstdisplay.Items.Add("Additional HDD 1 x 583.48")
+                lstdisplay.Items.Add(" " + "R583.48".PadLeft(220))
+
+                decSubtotal += 583.48D
+                decTotalMarkup += 583.48D * 0.4
+            Case 2
+                lstdisplay.Items.Add("Additional HDD 2 x 583.48")
+                lstdisplay.Items.Add(" " + "R1166.96".PadLeft(220))
+
+                decSubtotal += 1166.96D
+                decTotalMarkup += 1166.96D * 0.4
+            Case 3
+                lstdisplay.Items.Add("Additional HDD 3 x 583.48")
+                lstdisplay.Items.Add(" " + "R1750.44".PadLeft(220))
+
+                decSubtotal += 1750.44D
+                decTotalMarkup += 1750.44D * 0.4
+            Case 4
+                lstdisplay.Items.Add("Additional HDD 4 x 583.48")
+                lstdisplay.Items.Add(" " + "R2333.92".PadLeft(220))
+
+                decSubtotal += 2333.92D
+                decTotalMarkup += 2333.92D * 0.4
+            Case 5
+                lstdisplay.Items.Add("Additional HDD 5 x 583.48")
+                lstdisplay.Items.Add("R2917.40".PadLeft(220))
+
+                decSubtotal += 2917.4D
+                decTotalMarkup += 2917.4D * 0.4
+        End Select
+
+        'Calculate Total Sub, Total Markup, Vat Amount and Total
+        decVatAmount = (decTotalMarkup + decSubtotal) * decVatRate
+        dectotal = decSubtotal + decVatAmount + decTotalMarkup
+
+
+
+        'Display the Output
+        lstdisplay.Items.Add("___________________________________________________________________________________________________________________")
+        lstdisplay.Items.Add("Total Sub" + decSubtotal.ToString("C2", CultureInfo.CreateSpecificCulture("en-ZA")).PadLeft(204))
+        lstdisplay.Items.Add("Total Markup" + decTotalMarkup.ToString("C2", CultureInfo.CreateSpecificCulture("en-ZA")).PadLeft(200))
+        lstdisplay.Items.Add("VAT Amount" + decVatAmount.ToString("C2", CultureInfo.CreateSpecificCulture("en-ZA")).PadLeft(201))
+        lstdisplay.Items.Add("Total" + dectotal.ToString("C2", CultureInfo.CreateSpecificCulture("en-ZA")).PadLeft(212))
+
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Exit the Program
+        Me.Close()
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clearing the text boxes
+        txtName.Text = ""
+        txtSurname.Text = ""
+
+        'Clearing the radio buttons
+        radPersonalComputer.Checked = False
+        radLaptop.Checked = False
+        radTablet.Checked = False
+        radSmartphone.Checked = False
+
+        'Clearing the checkboxes
+        chkComputer.Checked = False
+        chkGraphicsCard.Checked = False
+        chkMonitor.Checked = False
+        chkKeyboard.Checked = False
+        chkMouse.Checked = False
+
+        'Reset the combo boxes
+        cmbAdditionalDDR4.SelectedIndex = 0
+        cmbAdditionalRam.SelectedIndex = 0
+
+        'Clearing the display
+        lstdisplay.Items.Clear()
+
+        'Resetting all the variables
+        strName = ""
+        strSurname = ""
+    End Sub
+    End Class
